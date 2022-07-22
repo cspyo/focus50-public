413d5b1c747d9168f49fdcfd7ecaacbb6cd10b2e
최승표 <cspyo98@naver.com>
2022-07-22 18:52:45 +0900

    Feat: 로고 누르면 홈 화면으로 이동하는 기능 구현 (FOC-94)

--- lib/widgets/header_logo.dart
+++ lib/widgets/header_logo.dart
@@ -1,30 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:focus42/consts/colors.dart';
+import 'package:focus42/consts/routes.dart';
+import 'package:get/get.dart';
 
 class HeaderLogo extends StatelessWidget {
   const HeaderLogo({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: const <Widget>[
-        Text(
-          'Focus',
-          style: TextStyle(
-            fontFamily: 'Okddung',
-            fontSize: 30,
-            color: Colors.black,
+    return InkWell(
+      onTap: () {
+        Get.rootDelegate.toNamed(Routes.ABOUT);
+      },
+      child: Row(
+        children: const <Widget>[
+          Text(
+            'Focus',
+            style: TextStyle(
+              fontFamily: 'Okddung',
+              fontSize: 30,
+              color: Colors.black,
+            ),
           ),
-        ),
-        Text(
-          '50',
-          style: TextStyle(
-            fontFamily: 'Okddung',
-            fontSize: 30,
-            color: purple300,
+          Text(
+            '50',
+            style: TextStyle(
+              fontFamily: 'Okddung',
+              fontSize: 30,
+              color: purple300,
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
