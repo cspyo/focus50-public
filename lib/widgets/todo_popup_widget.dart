8bb2350b391a5d6fd41dbad4bb48d525581b786e
y00njae <rladbswo12@gist.ac.kr>
2022-07-25 21:42:57 +0900

    Fix: 세션 투두 목록 버그 해결 (FOC-91)

--- lib/widgets/todo_popup_widget.dart
+++ lib/widgets/todo_popup_widget.dart
@@ -1,16 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:focus42/consts/colors.dart';
 import 'package:focus42/models/reservation_model.dart';
-import 'package:logger/logger.dart';
-
-import '../consts/colors.dart';
-import '../models/todo_model.dart';
-
-// import 'package:focus42/consts/colors.dart';
-// import 'package:todo/todo.dart';
-
-// ignore: camel_case_types
+import 'package:focus42/models/todo_model.dart';
 
 class TodoPopup extends StatefulWidget {
   final ReservationModel session;
@@ -27,143 +20,140 @@ class TodoPopupState extends State<TodoPopup> {
             fromFirestore: TodoModel.fromFirestore,
             toFirestore: (TodoModel todoModel, _) => todoModel.toFirestore(),
           );
-  late final Stream<QuerySnapshot> _myTodoColRef;
-  List<TodoModel> myTodo = [];
+  Map<String, TodoModel> myTodo = {};
+
   bool isEditing = false;
 
   TodoPopupState({required this.session});
 
   @override
   void initState() {
-    _myTodoColRef = _todoColRef
+    _todoColRef
         .where('userUid', isEqualTo: _user.currentUser?.uid)
         .where('isComplete', isEqualTo: false)
         .orderBy('completedDate')
         .orderBy('modifiedDate', descending: true)
         .orderBy('createdDate', descending: true)
-        .snapshots();
+        .snapshots()
+        .listen((QuerySnapshot querySnapshot) {
+      setState(() {
+        querySnapshot.docChanges.forEach((change) {
+          switch (change.type) {
+            case DocumentChangeType.added:
+              TodoModel todo = change.doc.data() as TodoModel;
+              todo.pk = change.doc.id;
+              myTodo[change.doc.id] = todo;
+              break;
+            case DocumentChangeType.removed:
+              myTodo.remove(change.doc.id);
+              break;
+            case DocumentChangeType.modified:
+              TodoModel todo = change.doc.data() as TodoModel;
+              todo.pk = change.doc.id;
+              myTodo[change.doc.id] = todo;
+              break;
+          }
+        });
+      });
+    });
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: _myTodoColRef,
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (snapshot.hasError) {
-          var logger = Logger();
-          logger.e(snapshot.error);
-          return Text('Something went wrong');
-        }
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Text("");
-        }
-        // var logger = Logger();
-        // logger.d("updated");
-        myTodo.clear();
-        snapshot.data!.docs.forEach((doc) {
-          // TODO: 효율적으로 todo list 보여주기
-          final TodoModel todo = doc.data() as TodoModel;
-          todo.pk = doc.id;
-          myTodo.add(todo);
-        });
-        return Container(
-          margin: EdgeInsets.only(top: 27),
-          width: 380,
-          child: Column(children: [
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                      margin: EdgeInsets.only(left: 10),
-                      child: Text('이번 세션에서 할 일을 선택해주세요 (최대 3개)',
-                          style: TextStyle(
-                              fontSize: 15, fontWeight: FontWeight.w600))),
-                  IconButton(
-                      onPressed: () {
-                        setState(() {
-                          isEditing = !isEditing;
-                        });
-                        // print(plus);
-                      },
-                      iconSize: 30,
-                      splashColor: Colors.transparent,
-                      hoverColor: Colors.transparent,
-                      icon: isEditing == false
-                          ? Icon(
-                              Icons.add,
-                              color: Colors.black,
-                            )
-                          : Icon(
-                              Icons.close,
-                              color: Colors.black,
-                            )),
-                ],
-              ),
-            ),
-            isEditing == true
-                ? Container(
-                    margin: EdgeInsets.only(top: 9),
-                    width: 360,
-                    height: 60,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(16),
-                    ),
-                    child: TextField(
-                        autofocus: true,
-                        textInputAction: TextInputAction.go,
-                        onSubmitted: (value) {
-                          DateTime now = DateTime.now();
-                          setState(() {
-                            isEditing = !isEditing;
-                            TodoModel todo = TodoModel(
-                              userUid: _user.currentUser!.uid,
-                              task: value,
-                              createdDate: now,
-                              modifiedDate: now,
-                              completedDate:
-                                  DateTime.fromMicrosecondsSinceEpoch(0),
-                              isComplete: false,
-                            );
-                            _todoColRef.add(todo);
-                          });
-                        },
-                        decoration: InputDecoration(
-                          hintText: '할 일을 적어주세요',
-                          fillColor: Colors.white,
-                          enabledBorder: UnderlineInputBorder(
-                            borderSide: const BorderSide(
-                                color: Colors.black, width: 2.0),
-                          ),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide: const BorderSide(
-                                color: Colors.black, width: 2.0),
-                          ),
-                        )))
-                : Text(''),
-            Expanded(
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    for (var i = 0; i < myTodo.length; i++)
-                      TodoPopupUi(
-                        task: myTodo[i].task!,
-                        isComplete: myTodo[i].isComplete!,
-                        createdDate: Timestamp.fromDate(myTodo[i].createdDate!),
-                        userUid: myTodo[i].userUid!,
-                        docId: myTodo[i].pk!,
-                        currentSessionId: session.pk!,
-                        assignedSessionId: myTodo[i].assignedSessionId,
-                      )
-                  ],
+    return Container(
+      margin: EdgeInsets.only(top: 27),
+      width: 380,
+      child: Column(children: [
+        Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                  margin: EdgeInsets.only(left: 10),
+                  child: Text('이번 세션에서 할 일을 선택해주세요 (최대 3개)',
+                      style: TextStyle(
+                          fontSize: 15, fontWeight: FontWeight.w600))),
+              IconButton(
+                  onPressed: () {
+                    setState(() {
+                      isEditing = !isEditing;
+                    });
+                  },
+                  iconSize: 30,
+                  splashColor: Colors.transparent,
+                  hoverColor: Colors.transparent,
+                  icon: isEditing == false
+                      ? Icon(
+                          Icons.add,
+                          color: Colors.black,
+                        )
+                      : Icon(
+                          Icons.close,
+                          color: Colors.black,
+                        )),
+            ],
+          ),
+        ),
+        isEditing == true
+            ? Container(
+                margin: EdgeInsets.only(top: 9),
+                width: 360,
+                height: 60,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(16),
                 ),
-              ),
-            )
-          ]),
-        );
-      },
+                child: TextField(
+                    autofocus: true,
+                    textInputAction: TextInputAction.go,
+                    onSubmitted: (value) {
+                      DateTime now = DateTime.now();
+                      setState(() {
+                        isEditing = !isEditing;
+                        TodoModel todo = TodoModel(
+                          userUid: _user.currentUser!.uid,
+                          task: value,
+                          createdDate: now,
+                          modifiedDate: now,
+                          completedDate: DateTime.fromMicrosecondsSinceEpoch(0),
+                          isComplete: false,
+                        );
+                        _todoColRef.add(todo);
+                      });
+                    },
+                    decoration: InputDecoration(
+                      hintText: '할 일을 적어주세요',
+                      fillColor: Colors.white,
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide:
+                            const BorderSide(color: Colors.black, width: 2.0),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide:
+                            const BorderSide(color: Colors.black, width: 2.0),
+                      ),
+                    )))
+            : Text(''),
+        Expanded(
+          child: SingleChildScrollView(
+            child: Column(
+              children: myTodo.values
+                  .map((element) => TodoPopupUi(
+                        task: element.task!,
+                        isComplete: element.isComplete!,
+                        createdDate: Timestamp.fromDate(element.createdDate!),
+                        userUid: element.userUid!,
+                        docId: element.pk!,
+                        currentSessionId: session.pk!,
+                        assignedSessionId: element.assignedSessionId,
+                      ))
+                  .whereType<Widget>()
+                  .toList(),
+            ),
+          ),
+        )
+      ]),
     );
   }
 }
@@ -189,8 +179,7 @@ class TodoPopupUi extends StatefulWidget {
     this.assignedSessionId,
   }) : super(key: Key);
   @override
-  State<TodoPopupUi> createState() => TodoPopupUiState(
-      assignedSessionId: assignedSessionId, currentSessionId: currentSessionId);
+  State<TodoPopupUi> createState() => TodoPopupUiState();
 }
 
 class TodoPopupUiState extends State<TodoPopupUi> {
@@ -199,16 +188,15 @@ class TodoPopupUiState extends State<TodoPopupUi> {
             fromFirestore: TodoModel.fromFirestore,
             toFirestore: (TodoModel todoModel, _) => todoModel.toFirestore(),
           );
-  final _user = FirebaseAuth.instance;
   bool isHover = false;
   bool isEdit = false;
-  String? assignedSessionId;
-  final String? currentSessionId;
   void onHover(PointerEvent details) {
     setState(() {
       isHover = true;
     });
   }
+
+  TodoPopupUiState();
 
   void onExit(PointerEvent details) {
     setState(() {
@@ -216,15 +204,9 @@ class TodoPopupUiState extends State<TodoPopupUi> {
     });
   }
 
-  TodoPopupUiState({
-    required this.assignedSessionId,
-    required this.currentSessionId,
-  });
-
   @override
   Widget build(BuildContext context) {
     String task = widget.task;
-    bool isComplete = widget.isComplete;
 
     return Container(
         margin: EdgeInsets.only(top: 9),
@@ -252,27 +234,10 @@ class TodoPopupUiState extends State<TodoPopupUi> {
               child: Row(
                 children: [
                   TextButton(
-                      onPressed: () {
-                        setState(() {
-                          TodoModel newTodo;
-                          if (assignedSessionId != currentSessionId) {
-                            newTodo = TodoModel(
-                              assignedSessionId: currentSessionId,
-                            );
-                            assignedSessionId = currentSessionId;
-                          } else {
-                            newTodo = TodoModel(
-                              assignedSessionId: null,
-                            );
-                            assignedSessionId = null;
-                          }
-                          _todoColRef
-                              .doc(widget.docId)
-                              .update(newTodo.toUpdateFirestore());
-                        });
-                      },
-                      child: assignedSessionId != null &&
-                              assignedSessionId == currentSessionId
+                      onPressed: setAssignedSession,
+                      child: widget.assignedSessionId != null &&
+                              widget.assignedSessionId ==
+                                  widget.currentSessionId
                           ? Icon(Icons.check, color: Colors.black)
                           : Icon(Icons.check_box_outline_blank_outlined,
                               color: Colors.black)),
@@ -280,5 +245,21 @@ class TodoPopupUiState extends State<TodoPopupUi> {
                 ],
               ),
             )));
+  }
+
+  void setAssignedSession() {
+    return setState(() {
+      TodoModel newTodo;
+      if (widget.assignedSessionId != widget.currentSessionId) {
+        newTodo = TodoModel(
+          assignedSessionId: widget.currentSessionId,
+        );
+      } else {
+        newTodo = TodoModel(
+          assignedSessionId: null,
+        );
+      }
+      _todoColRef.doc(widget.docId).update(newTodo.toUpdateFirestore());
+    });
   }
 }
