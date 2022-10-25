ce94a30098d8b31b9247a878a8bea112408dfc9c
y00njae <rladbswo12@gist.ac.kr>
2022-10-25 17:07:40 +0900

    Feat: 쿠키 여부에 따라 팝업창 보여주기 코드 구현 완료

---
+++ lib/feature/popup/presentation/notice_popup.dart
@@ -0,0 +1,17 @@
+import 'dart:html' as html;
+
+import 'package:focus42/feature/popup/view_model/cookie_utils.dart';
+
+//* Ex) 주소/MainPop.do?popType=1&popSeq=12193
+//* 참조 - 4대 보험 제출
+
+void launchNoticeWindow() {
+  final popSeq = 1;
+  final popName = "Focus50 - 알려드립니다";
+  final Map<String, String> cookie = getCookie();
+  if (cookie['mainPop${popSeq}'] == "close") {
+    return;
+  }
+  html.window.open("mainPop.html?popSeq=${popSeq}", popName,
+      'left=100,top=100,height=570,width=520');
+}
