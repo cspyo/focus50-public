3a83ae2d321db8ed1a40644788192ca172849dc0
y00njae <rladbswo12@gist.ac.kr>
2022-10-28 06:55:04 +0900

    Fix: 로그아웃 이후 활성 그룹 변경

--- lib/view_models.dart/reservation_view_model.dart
+++ lib/view_models.dart/reservation_view_model.dart
@@ -12,7 +12,11 @@ import 'package:focus42/view_models.dart/users_notifier.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 final activatedGroupIdProvider = StateProvider<String>((ref) {
-  final _ret = "public";
+  String _ret = "public";
+  final database = ref.watch(databaseProvider);
+  if (database.uid == 'none') {
+    _ret = "public";
+  }
   return _ret;
 });
 
