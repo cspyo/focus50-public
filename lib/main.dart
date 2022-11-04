eca2568d19e703c47a9ea20a9046be836e3e631e
y00njae <rladbswo12@gist.ac.kr>
2022-11-05 03:35:45 +0900

    Chore: 릴리즈 1.7.6

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ import 'package:url_strategy/url_strategy.dart';
 import 'consts/app_router_delegate.dart';
 import 'firebase_options.dart';
 
-const String VERSION = "1.7.5";
+const String VERSION = "1.7.6";
 late final AGENT;
 
 Future<void> versionCheck() async {
